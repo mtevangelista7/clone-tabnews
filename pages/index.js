11409f97ef8daee9db033a4f2b3749d0077ddf99
mtevangelista7 <matheus.evangelista7@protonmail.com>
2025-11-10 19:13:54 -0300

    feat: create project

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <div style={{height: "100vh", margin: "0", display: "flex", justifyContent: "center", alignItems: "center", background: "black"}}>
+        <h1 style={{margin: "0", color:"white"}}>Oi</h1>
+    </div>
+}
+
+export default Home;
